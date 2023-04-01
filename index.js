6704c24813c1232ea3bf70f7434dc38771aae3e5
BogdanGlazkov <bglazkov@i.ua>
2023-04-01 21:25:49 +0300

    Add orders

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const homeRoutes = require("./routes/home");
 const cartRoutes = require("./routes/cart");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const ordersRoutes = require("./routes/orders");
 const User = require("./models/userModel");
 
 const app = express();
@@ -44,6 +45,7 @@ app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
 app.use("/cart", cartRoutes);
+app.use("/orders", ordersRoutes);
 
 async function start() {
   try {
