fa909a646854afd0042c0b3a59961824faaa0ba3
BogdanGlazkov <bglazkov@i.ua>
2023-04-05 00:23:24 +0300

    Add course validation

--- routes/add.js
+++ routes/add.js
@@ -1,15 +1,28 @@
 const { Router } = require("express");
+const { validationResult } = require("express-validator");
 const Course = require("../models/courseModel");
 const auth = require("../middlewares/auth");
+const { courseValidators } = require("../utils/validators");
 const router = Router();
 
 router.get("/", auth, (req, res) => {
   res.render("add", { title: "Add course", isAdd: true });
 });
 
-router.post("/", auth, async (req, res) => {
+router.post("/", auth, courseValidators, async (req, res) => {
+  const { title, price, img } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("add", {
+      title: "Add course",
+      isAdd: true,
+      error: errors.array()[0].msg,
+      data: { title, price, img },
+    });
+  }
+
   try {
-    const { title, price, img } = req.body;
     const course = new Course({ title, price, img, userId: req.user._id });
     await course.save();
 
--- utils/validators.js
+++ utils/validators.js
@@ -14,18 +14,30 @@ exports.registerValidators = [
       } catch (error) {
         console.log(error);
       }
-    }),
-  body(
-    "password",
-    "Password must contain from 6 to 56 alphanumeric characters"
-  ).isLength({ min: 6, max: 56 }),
-  body("confirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Password doesn't match");
-    }
-    return true;
-  }),
+    })
+    .normalizeEmail(),
+  body("password", "Password must contain from 6 to 56 alphanumeric characters")
+    .isLength({ min: 6, max: 56 })
+    .trim(),
+  body("confirm")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password doesn't match");
+      }
+      return true;
+    })
+    .trim(),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("Name must contain at least 3 characters"),
+    .withMessage("Name must contain at least 3 characters")
+    .trim(),
+];
+
+exports.courseValidators = [
+  body("title")
+    .isLength({ min: 3 })
+    .withMessage("Min title length is 3 characters")
+    .trim(),
+  body("price").isNumeric().withMessage("Enter valid price"),
+  body("img", "Enter valid image URL address").isURL(),
 ];
